fbec7512858abcff7e9f3b92e8d82f9a653b8892
yuhongjie <yuhongjie@sogou-inc.com>
2020-10-21 16:53:41 +0800

    modify reverse edge

--- dgraph/cmd/bulk/schema.go
+++ dgraph/cmd/bulk/schema.go
@@ -207,17 +207,6 @@ func (s *schemaStore) write(db *badger.DB, preds []string) {
 			TypeName: typ.TypeName,
 		}
 		var typePredMap = make(map[string]struct{})
-		for pred, _ := range s.schemaMap {
-			if strings.Contains(pred, "dgraph") {
-				continue
-			}
-			if _, ok := typePredMap[pred]; ok {
-				continue
-			}
-			schema := &pb.SchemaUpdate{Predicate: pred}
-			thingSystem.Fields = append(thingSystem.Fields, schema)
-			typePredMap[pred] = struct{}{}
-		}
 
 		for _, pred := range typ.Fields {
 			if strings.Contains(pred.Predicate, "dgraph") {
@@ -229,6 +218,24 @@ func (s *schemaStore) write(db *badger.DB, preds []string) {
 			schema := &pb.SchemaUpdate{Predicate: pred.Predicate}
 			thingSystem.Fields = append(thingSystem.Fields, schema)
 			typePredMap[pred.Predicate] = struct{}{}
+		}
+
+		for pred, updateSchema := range s.schemaMap {
+			if strings.Contains(pred, "dgraph") {
+				continue
+			}
+			if updateSchema.Directive == pb.SchemaUpdate_REVERSE {
+				predd := "~" + pred
+				schema := &pb.SchemaUpdate{Predicate: predd}
+				thingSystem.Fields = append(thingSystem.Fields, schema)
+				typePredMap[predd] = struct{}{}
+			}
+			if _, ok := typePredMap[pred]; ok {
+				continue
+			}
+			schema := &pb.SchemaUpdate{Predicate: pred}
+			thingSystem.Fields = append(thingSystem.Fields, schema)
+			typePredMap[pred] = struct{}{}
 		}
 		typsTemp = append(typsTemp, thingSystem)
 	}
